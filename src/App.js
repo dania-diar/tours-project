50b6770ff9043bdfa104ce7f566ccba6b884a30b
Dania <daniadeyar@gmail.com>
2022-01-22 23:34:01 +0300

    formatting

--- src/App.js
+++ src/App.js
@@ -1,66 +1,61 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  const [loading,setLoading]= useState(true);
-  const [tours,setTours]= useState([]);
-  const removeTour=(id)=>{
-    const newtours = tours.filter((tour)=> tour.id!==id);
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const removeTour = (id) => {
+    const newtours = tours.filter((tour) => tour.id !== id);
     setTours(newtours);
-  }
- 
-  const fetchTours= async()=>{
- setLoading(true);
+  };
 
- try {
-const response = await fetch(url)
- const tours= await response.json();
- setLoading(false)
- setTours(tours)
-   
- } catch (error) {
-   setLoading(false)
-   console.log(error)
- }
- 
- 
+  const fetchTours = async () => {
+    setLoading(true);
 
-  }
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
 
-  useEffect(()=>{
-fetchTours()
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
-  },[])
-
-
-  if(loading){
-    return <main>
-     
-     <Loading/>
-
-    </main>
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
   }
 
-  if(tours.length===0){
-    return <main>
-      <div className='title'>
-        <h2> NO tours left</h2>
-        <button   className='btn'  onClick={()=>fetchTours()}> refresh Tours</button>
-      </div>
-    </main>
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2> NO tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            {" "}
+            refresh Tours
+          </button>
+        </div>
+      </main>
+    );
   }
 
   return (
     <main>
-  <Tours tours={tours}  removeTour={removeTour}/>
-
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
-  )
-  
-  
+  );
 }
 
-export default App
+export default App;
